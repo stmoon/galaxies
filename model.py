e9aa2c8aa8aa41e39bd59af753cf744fb56b0646
stmoon <munhoney@gmail.com>
2017-06-01 17:27:11 +0900

    update initial value

--- model.py
+++ model.py
@@ -8,6 +8,8 @@ import numpy as np
 import cv2, os
 import prepare_data
 import csv
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 prepare_data.prepre_resize_train()      ## Create Train Data
@@ -19,10 +21,9 @@ prepare_data.prepare_csv()
 IMAGE_WIDTH =  128
 IMAGE_HEIGHT = 128
 KEEP_PROB = 0.7
-LEARNING_RATE = 1e-3
-TRAIN_EPOCH = 10 #1000
-BATCH_SIZE = 10 #50
-NUM_TOTAL_TRAINING_DATA = 100 #61578
+TRAIN_EPOCH = 500
+BATCH_SIZE = 50
+NUM_TOTAL_TRAINING_DATA = 61578
 NUM_THREADS = 4
 CAPACITY = 50000
 MIN_AFTER_DEQUEUE = 100
@@ -31,9 +32,10 @@ FILTER_SIZE = 2
 POOLING_SIZE = 2
 
 
-def conv_layer(input, size_in, size_out, name="conv", stddev=0.01):
+def conv_layer(input, size_in, size_out, name="conv"):
   with tf.name_scope(name):
-    w = tf.Variable(tf.truncated_normal([FILTER_SIZE, FILTER_SIZE, size_in, size_out], stddev=stddev), name="W")
+    w_init = tf.contrib.layers.xavier_initializer_conv2d()
+    w = tf.get_variable(name+"_w", shape=[FILTER_SIZE, FILTER_SIZE, size_in, size_out], initializer=w_init)
     b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
     conv = tf.nn.conv2d(input, w, strides=[1, 1, 1, 1], padding="SAME")
     act = tf.nn.relu(conv + b)
@@ -43,10 +45,11 @@ def conv_layer(input, size_in, size_out, name="conv", stddev=0.01):
     return tf.nn.max_pool(act, ksize=[1, POOLING_SIZE, POOLING_SIZE, 1], strides=[1, 2, 2, 1], padding="SAME")
 
 
-def fc_layer(input, size_in, size_out, name="fc", stddev=0.01):
+def fc_layer(input, size_in, size_out, name="fc"):
   with tf.name_scope(name):
     flat_input = tf.reshape(input, [-1, size_in])
-    w = tf.Variable(tf.truncated_normal([size_in, size_out], stddev=stddev), name="W")
+    w_init = tf.contrib.layers.xavier_initializer();
+    w = tf.get_variable(name+"_w", shape=[size_in, size_out], initializer=w_init)
     b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
     act = tf.nn.relu(tf.matmul(flat_input, w) + b)
     tf.summary.histogram(name+"_weights", w)
@@ -55,7 +58,7 @@ def fc_layer(input, size_in, size_out, name="fc", stddev=0.01):
     return act
 
 
-def model(learning_rate, std_dev, hparam) :
+def model(learning_rate, hparam) :
 
     # open session
     tf.reset_default_graph()
@@ -90,26 +93,33 @@ def model(learning_rate, std_dev, hparam) :
 
 
     ### Graph 
-    conv1 = conv_layer(X, 3, 32, name='conv1', stddev=std_dev)
-    conv2 = conv_layer(conv1, 32, 64, name='conv2', stddev=std_dev)
-    conv3 = conv_layer(conv2, 64, 128, name='conv3', stddev=std_dev )
-    conv4 = conv_layer(conv3, 128, 256, name='conv4', stddev=std_dev)
-    conv5 = conv_layer(conv4, 256, 512, name='conv5', stddev=std_dev)
-    conv6 = conv_layer(conv5, 512, 1024, name='conv6', stddev=std_dev)
-    conv7 = conv_layer(conv6, 1024, 2048, name='conv7', stddev=std_dev)
-    fc1   = fc_layer(conv7, 2048, 2048, name='fc1', stddev=std_dev)
-    fc2   = fc_layer(fc1, 2048, 3, name='fc2', stddev=std_dev)
+    conv1 = conv_layer(X, 3, 32, name='conv1')
+    conv2 = conv_layer(conv1, 32, 64, name='conv2')
+    conv3 = conv_layer(conv2, 64, 128, name='conv3')
+    conv4 = conv_layer(conv3, 128, 256, name='conv4')
+    conv5 = conv_layer(conv4, 256, 512, name='conv5')
+    conv6 = conv_layer(conv5, 512, 1024, name='conv6')
+    conv7 = conv_layer(conv6, 1024, 2048, name='conv7')
+    fc1   = fc_layer(conv7, 2048, 2048, name='fc1')
+    fc2   = fc_layer(fc1, 2048, 3, name='fc2')
     logits = fc2
 
     with tf.name_scope('xent') :
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y)) 
 	cost_hist = tf.summary.scalar('cost', cost) 
+
     with tf.name_scope('train') :
 	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
+    with tf.name_scope('distance') :
+	dist = tf.reduce_mean(tf.square(tf.subtract(logits, Y)), axis=1)
+	dist = tf.reduce_mean(tf.sqrt(dist))
+	dist_hist = tf.summary.scalar('distance', dist)
+
+
     with tf.name_scope('accuracy') :
-	accuracy = tf.reduce_mean(tf.square(tf.subtract(logits, Y)), axis=1)
-	accuracy = tf.reduce_mean(tf.sqrt(accuracy))
+	correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(Y,1))	
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 	acc_hist = tf.summary.scalar('accuracy', accuracy)
 
     summary  = tf.summary.merge_all()
@@ -126,19 +136,18 @@ def model(learning_rate, std_dev, hparam) :
 	for i in range(total_batch):
 	    batch_x, batch_y = sess.run([image_batch, label_batch])
 	    batch_y = batch_y.reshape(BATCH_SIZE, NUM_CLASSES)
-	    cost_value, _, summ, acc = sess.run([cost, optimizer, summary, accuracy], feed_dict={X: batch_x, Y: batch_y})
+	    cost_value, _, summ, dist1, acc = sess.run([cost, optimizer, summary, dist, accuracy], feed_dict={X: batch_x, Y: batch_y})
 
 	writer.add_summary(summ, epoch)
 	print "epoch[%d] : %f " % (epoch, acc) 
+        
 	    
     coord.request_stop()
     coord.join(threads) 
 
 def main() :
-    for learning_rate in [1E-2, 1E-3, 1E-4, 1E-5]:
-	for std_dev in [1E-1, 1E-2, 1E-3] :
-	    model(learning_rate, std_dev, "param_%f_%f" % (learning_rate, std_dev))
+    for learning_rate in [ 4.0*1E-4] :
+	model(learning_rate, "param_%f" % (learning_rate))
 
 if __name__ == '__main__':
-  main()
-
+    main()
